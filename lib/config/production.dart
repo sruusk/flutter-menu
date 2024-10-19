36ab62fa96421a294699eb17694b57e0c7543fd0
sruusk <57395313+sruusk@users.noreply.github.com>
2024-10-19 20:31:38 +0300

    Better error handling

--- lib/config/production.dart
+++ lib/config/production.dart
@@ -1,2 +1,2 @@
 // lib/config/production.dart
-const String baseUrl = 'https://ls804gkso40oswswsgkw0840.a32.fi/api/menu';
+const String baseUrl = 'https://ouf.fi/api/menu';
--- lib/widgets/settings_sheet.dart
+++ lib/widgets/settings_sheet.dart
@@ -27,7 +27,8 @@ class DateBottomSheet extends StatefulWidget {
   State<DateBottomSheet> createState() => _DateBottomSheetState();
 }
 
-class _DateBottomSheetState extends State<DateBottomSheet> with SingleTickerProviderStateMixin {
+class _DateBottomSheetState extends State<DateBottomSheet>
+    with SingleTickerProviderStateMixin {
   bool updateAvailable = false;
   late Updater updater;
   late AnimationController _controller;
@@ -58,168 +59,195 @@ class _DateBottomSheetState extends State<DateBottomSheet> with SingleTickerProv
     final preferencesNotifier = Provider.of<PreferencesNotifier>(context);
 
     return SingleChildScrollView(
-      child: FutureBuilder<List<Tuple2<DateTime, String>>>(
-        future: widget.availableDates,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Container(
-              padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
+      child: Container(
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Text(
-                          AppLocalizations.of(context).translate('settings.settings'),
-                          style: Theme.of(context).textTheme.headlineSmall,
-                        ),
-                      ],
-                    ),
+                  Text(
+                    AppLocalizations.of(context).translate('settings.settings'),
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headlineSmall,
                   ),
-                  SettingsCard(
-                    title: "${AppLocalizations.of(context).translate('settings.selectDay')}: ",
+                ],
+              ),
+            ),
+            FutureBuilder<List<Tuple2<DateTime, String>>>(
+              future: widget.availableDates,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return SettingsCard(
+                    title: "${AppLocalizations.of(context).translate(
+                        'settings.selectDate')}: ",
                     child: MenuAnchorWidget(
                       menuChildren: [
                         for (var dateOption in snapshot.data!)
                           MenuItemButton(
-                            child: Text(AppLocalizations.of(context).translate('weekdays.${MenuApi.getDayOfWeek(dateOption.item1.weekday)}')),
+                            child: Text(dateOption.item2),
                             onPressed: () {
                               widget.setDate(dateOption.item1);
                               Navigator.pop(context);
                             },
                           ),
                       ],
-                      buttonText: AppLocalizations.of(context).translate('weekdays.${MenuApi.getDayOfWeek(widget.date.weekday)}'),
+                      buttonText: widget.date.toString().substring(0, 10),
                       onPressed: () {},
                     ),
-                  ),
-                  FutureBuilder(
-                    future: widget.campuses,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return SettingsCard(
-                          title: "${AppLocalizations.of(context).translate('settings.selectCampus')}: ",
-                          child: MenuAnchorWidget(
-                            menuChildren: [
-                              for (var campus in snapshot.data!)
-                                MenuItemButton(
-                                  child: Text(campus.toString()),
-                                  onPressed: () {
-                                    preferencesNotifier.setPreference('campus', campus.toString());
-                                    Navigator.pop(context);
-                                  },
-                                ),
-                            ],
-                            buttonText: preferencesNotifier.value.preferences['campus'] ?? '',
-                            onPressed: () {},
-                          ),
-                        );
-                      } else {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                    },
-                  ),
-                  SettingsCard(
-                    title: "${AppLocalizations.of(context).translate('settings.selectLang')}: ",
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+            FutureBuilder(
+              future: widget.campuses,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return SettingsCard(
+                    title: "${AppLocalizations.of(context).translate(
+                        'settings.selectCampus')}: ",
                     child: MenuAnchorWidget(
                       menuChildren: [
-                        MenuItemButton(
-                          child: const Text('English'),
-                          onPressed: () {
-                            preferencesNotifier.setPreference('language_code', 'en');
-                            Navigator.pop(context);
-                          },
-                        ),
-                        MenuItemButton(
-                          child: const Text('Suomi'),
-                          onPressed: () {
-                            preferencesNotifier.setPreference('language_code', 'fi');
-                            Navigator.pop(context);
-                          },
-                        ),
+                        for (var campus in snapshot.data!)
+                          MenuItemButton(
+                            child: Text(campus.toString()),
+                            onPressed: () {
+                              preferencesNotifier.setPreference(
+                                  'campus', campus.toString());
+                              Navigator.pop(context);
+                            },
+                          ),
                       ],
-                      buttonText: AppLocalizations.of(context).translate('settings.${AppLocalizations.of(context).locale.languageCode}'),
+                      buttonText: preferencesNotifier.value
+                          .preferences['campus'] ?? '',
                       onPressed: () {},
                     ),
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+            SettingsCard(
+              title: "${AppLocalizations.of(context).translate(
+                  'settings.selectLang')}: ",
+              child: MenuAnchorWidget(
+                menuChildren: [
+                  MenuItemButton(
+                    child: const Text('English'),
+                    onPressed: () {
+                      preferencesNotifier.setPreference('language_code', 'en');
+                      Navigator.pop(context);
+                    },
                   ),
-                  FutureBuilder(
-                    future: updater.currentVersion,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return SettingsCard(
-                          title: "${AppLocalizations.of(context).translate('settings.version')}: ",
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                              backgroundColor: WidgetStateProperty.all<Color>(Theme.of(context).colorScheme.surfaceBright),
-                              shadowColor: WidgetStateProperty.all<Color>(Theme.of(context).colorScheme.shadow),
-                              elevation: WidgetStateProperty.all<double>(2),
-                            ),
-                            onPressed: () async {
-                              _controller.repeat();
-                              bool isUpdateAvailable = await updater.isUpdateAvailable(forceCheck: true);
-                              setState(() {
-                                updateAvailable = isUpdateAvailable;
-                              });
-                              _controller.stop();
-                              _controller.forward();
-                            },
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(updateAvailable ? AppLocalizations.of(context).translate('update.update') : snapshot.data.toString(), style: Theme.of(context).textTheme.titleMedium),
-                                const SizedBox(width: 8),
-                                RotationTransition(
-                                  turns: _animation,
-                                  child: updateAvailable ? const Icon(Icons.download) : const Icon(Icons.refresh),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      } else {
-                        return const Center(child: CircularProgressIndicator());
-                      }
+                  MenuItemButton(
+                    child: const Text('Suomi'),
+                    onPressed: () {
+                      preferencesNotifier.setPreference('language_code', 'fi');
+                      Navigator.pop(context);
                     },
                   ),
                 ],
+                buttonText: AppLocalizations.of(context).translate(
+                    'settings.${AppLocalizations
+                        .of(context)
+                        .locale
+                        .languageCode}'),
+                onPressed: () {},
               ),
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+            ),
+            FutureBuilder(
+              future: updater.currentVersion,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return SettingsCard(
+                    title: "${AppLocalizations.of(context).translate(
+                        'settings.version')}: ",
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all<Color>(Theme
+                            .of(context)
+                            .colorScheme
+                            .surfaceBright),
+                        shadowColor: WidgetStateProperty.all<Color>(Theme
+                            .of(context)
+                            .colorScheme
+                            .shadow),
+                        elevation: WidgetStateProperty.all<double>(2),
+                      ),
+                      onPressed: () async {
+                        _controller.repeat();
+                        bool isUpdateAvailable = await updater
+                            .isUpdateAvailable(forceCheck: true);
+                        setState(() {
+                          updateAvailable = isUpdateAvailable;
+                        });
+                        _controller.stop();
+                        _controller.forward();
+                      },
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(updateAvailable ? AppLocalizations.of(context)
+                              .translate('update.update') : snapshot.data
+                              .toString(), style: Theme
+                              .of(context)
+                              .textTheme
+                              .titleMedium),
+                          const SizedBox(width: 8),
+                          RotationTransition(
+                            turns: _animation,
+                            child: updateAvailable
+                                ? const Icon(Icons.download)
+                                : const Icon(Icons.refresh),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-Future<void> showDateBottomSheet(
-  BuildContext context,
-  Future<List<Tuple2<DateTime, String>>> availableDates,
-  Future<List<Campus>> campuses,
-  Function(DateTime) setDate,
-  MenuApi api,
-  DateTime date,
-) {
-  return showModalBottomSheet(
-    context: context,
-    showDragHandle: true,
-    isScrollControlled: true,
-    enableDrag: true,
-    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-    builder: (BuildContext builder) {
-      return DateBottomSheet(
-        availableDates: availableDates,
-        campuses: campuses,
-        setDate: setDate,
-        api: api,
-        date: date,
-      );
-    },
-  );
-}
+  Future<void> showDateBottomSheet(BuildContext context,
+      Future<List<Tuple2<DateTime, String>>> availableDates,
+      Future<List<Campus>> campuses,
+      Function(DateTime) setDate,
+      MenuApi api,
+      DateTime date,) {
+    return showModalBottomSheet(
+      context: context,
+      showDragHandle: true,
+      isScrollControlled: true,
+      enableDrag: true,
+      backgroundColor: Theme
+          .of(context)
+          .scaffoldBackgroundColor,
+      builder: (BuildContext builder) {
+        return DateBottomSheet(
+          availableDates: availableDates,
+          campuses: campuses,
+          setDate: setDate,
+          api: api,
+          date: date,
+        );
+      },
+    );
+  }
